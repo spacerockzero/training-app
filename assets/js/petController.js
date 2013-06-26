5a10aa229eb3632eee95187856f408fa78e997f8
spacerockzero <jakob@jakobanderson.com>
2013-06-26 09:45:52 -0600

    added angular stuff for hello pets view

--- assets/js/petController.js
+++ assets/js/petController.js
@@ -1,12 +1,34 @@
 function PetListCtrl($scope) {
+
   $scope.pets = [
 		{
-			'name': 'Paikea',
-			'breed': 'Pomeranian'
+			name: 'Paikea',
+			breed: 'Pomeranian'
 		},
 		{
-			'name': "Apollo",
-			'breed': 'Australian Shepherd'
+			name: "Apollo",
+			breed: 'Australian Shepherd'
 		}
 	];
+
+	$scope.getTotalPets = function() {
+		return $scope.pets.length;
+	};
+
+	$scope.addPet = function(item){
+		$scope.pets.push(item);
+		$scope.newPet = {};
+		console.log($scope.pets);
+	}
+
+	$scope.removePet = function(pet){
+		$scope.pets.splice($scope.pets.indexOf(pet),1)
+	}
+
+	$scope.editPet = function(pet){
+		var i = $scope.pets.indexOf(pet);
+		$scope.pets[i] = pet;
+		console.log($scope.pets);
+	}
+
 }
