c92736fb8bd76ad263fcd865c8b8b5e2cfc31503
spacerockzero <jakob@jakobanderson.com>
2013-07-19 16:34:35 -0600

    angular tut step

--- assets/js/frontier/angularDemo.js
+++ assets/js/frontier/angularDemo.js
@@ -10,10 +10,59 @@
 
 var demoApp = angular.module('demoApp', []);
 
+demoApp.config( function( $routeProvider ){
+	$routeProvider
+		.when('/view1',
+		  {
+		  	controller: 'SimpleController',
+		  	template: '<div class="container">\
+										<h2>View 1</h2>\
+										Name:\
+										<br/>\
+										<input type="text" ng-model="name" />\
+										<br/>\
+										<ul>\
+											<li ng-repeat="cust in customers | filter:name">\
+												{{ cust.name | uppercase }} - {{ cust.city | lowercase }}\
+											</li>\
+										</ul>\
+										Customer Name: <br/>\
+										<input type="text" ng-model="newCustomer.name">\
+										Customer City: <br/>\
+										<input type="text" ng-model="newCustomer.city">\
+										<br/>\
+										<button ng-click="addCustomer()">Add Customer</button>\
+									</div>'
+		  })
+		.when('/view2',
+      {
+      	controller: 'SimpleController',
+      	template: '<div class="container">\
+										<h2>View 2</h2>\
+										Name:\
+										<br/>\
+										<input type="text" ng-model="name" />\
+										<br/>\
+										<ul>\
+											<li ng-repeat="cust in customers | filter:name">\
+												{{ cust.name | uppercase }} - {{ cust.city | lowercase }}\
+											</li>\
+										</ul>\
+										Customer Name: <br/>\
+										<input type="text" ng-model="newCustomer.name">\
+										Customer City: <br/>\
+										<input type="text" ng-model="newCustomer.city">\
+										<br/>\
+										<button ng-click="addCustomer()">Add Customer</button>\
+									</div>'
+      })
+		.otherwise({ redirectTo: '/' });
+});
 
 
-function SimpleController ( $scope ) {
+var controllers = {};
 
+controllers.SimpleController = function( $scope ) {
 	$scope.customers = [
 		{ name: 'John Smith', city: 'Phoenix' },
 		{ name: 'Sven Heimer', city: 'Asgaard' },
@@ -22,8 +71,13 @@ function SimpleController ( $scope ) {
 		{ name: 'Aarkandon Onandon', city: 'Bickspacefal' },
 		{ name: 'Gerschwinn Humblemattr', city: 'Glasgow' }
 	];
+	$scope.addCustomer = function(){
+		$scope.customers.push(
+		  {
+		  	name: $scope.newCustomer.name,
+		  	city: $scope.newCustomer.city
+		  });
+	}
+};
 
-}
-
-
-demoApp.controller( 'SimpleController', SimpleController );
+demoApp.controller( controllers );
