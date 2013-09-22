081fe61dd86781d9064b661d7f2c58513afe4f7d
spacerockzero <jakob@jakobanderson.com>
2013-09-22 16:59:46 -0600

    finished javascript apprentice badge progress

--- assets/js/javascript/apprentice.js
+++ assets/js/javascript/apprentice.js
@@ -42,7 +42,8 @@ var elements = {
 			privateMethodElement: document.getElementById('private-function'),
 			publicInterfaceElement: document.getElementById('public-interface'),
 			playNameElement: document.getElementById('play-name'),
-			playLifeStoryElement: document.getElementById('play-life-story')
+			playLifeStoryElement: document.getElementById('play-life-story'),
+			playNickNameElement: document.getElementById('play-nick-name')
 		}
 		getters = {
 			getPrivateData: function(){
@@ -64,17 +65,17 @@ function putPrivateData(){
 	elements.privateDataElement.textContent = getters.getPrivateData();
 }
 function putPrivateMethod(){
-	try {
-		if (!privateMethod || typeof privateMethod != 'function'){
-			throw 'privateMethod does not exist or not accessible from this scope';
-		}
-	}
-	catch (err){
-		console.log('privateMethod ERROR:',err)
-		if (typeof privateMethod != 'function'){
-			throw 'privateMethod ERROR: privateMethod does not exist or not accessible from this scope';
-		}
-	}
+	// try {
+	// 	if (!privateMethod || typeof privateMethod != 'function'){
+	// 		throw 'privateMethod does not exist or not accessible from this scope';
+	// 	}
+	// }
+	// catch (err){
+	// 	console.log('privateMethod ERROR:',err)
+	// 	if (typeof privateMethod != 'function'){
+	// 		throw 'privateMethod ERROR: privateMethod does not exist or not accessible from this scope';
+	// 	}
+	// }
 	// console.log('getters.getPrivateMethod()',getters.getPrivateMethod())
 	// elements.privateMethodElement.textContent = getters.getPrivateMethod();
 }
@@ -100,9 +101,21 @@ var personData = {
 	getLifeStory: function(){
 		return $('#lifeStory').val();
 	},
+	getNickName: function(){
+		return $('#nickName').val();
+	},
 	getNewPerson: function(){
-		personData.name = personData.getName();
-		personData.lifeStory = personData.getLifeStory();
+		if (!personData.filterNickName(personData.getNickName()) && !personData.filterName(personData.getName()) && !personData.filterText(personData.getLifeStory())){
+			$('#bad-words-alert').hide()
+			console.log('all words used check out');
+			personData.name = personData.getName();
+			personData.lifeStory = personData.getLifeStory();
+			personData.nickName = personData.getNickName();
+		}
+		else {
+			$('#bad-words-alert').show()
+			console.log('there was an inappropriate word in one or more input fields, please correct it.')
+		}
 	},
 	logPerson: function(){
 		console.log( "NAME:",personData.name,"LIFESTORY:",personData.lifeStory );
@@ -110,15 +123,49 @@ var personData = {
 	putNewPerson: function(){
 		elements.playNameElement.textContent = personData.name;
 		elements.playLifeStoryElement.textContent = personData.lifeStory;
+		elements.playNickNameElement.textContent = personData.nickName;
+	},
+	filterName: function(text){
+		return offensiveWordsPattern.test(text);
+	},
+	filterText: function(text){
+		return offensiveWordsPattern.test(text);
+	},
+	filterNickName: function(text){
+		return offensiveWordsPattern.test(text);
 	}
-}
+};
+var offensiveWordsPattern = new RegExp(/(stinky|gross|messy|nasty|smelly)/g);
 
 // form-handling
 $(document).ready(function(){
 	$('input,textarea').on('keyup', function(e){
 		e.preventDefault();
+		console.log('inside binding event')
 		personData.getNewPerson();
 		personData.logPerson();
 		personData.putNewPerson();
 	});
 });
+
+var outerFunction = function(){
+	var innerFunction = function(){
+		var objectThing = {
+			a: 2,
+			b: 3,
+			c: 'simple'
+		}
+		return objectThing;
+	}
+	console.log('innerFunction:',innerFunction());
+}
+outerFunction();
+
+var Car = function(){
+	this.wheels = 4;
+};
+Car.prototype.hood = true;
+
+var toyota = new Car();
+toyota.color = 'blue';
+console.log('toyota:',toyota);
